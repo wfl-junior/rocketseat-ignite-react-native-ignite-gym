ff39dd45229892ab4ee915a203a96e2deaf2482a
Wallace Júnior <wflj1997@gmail.com>
2023-04-08 17:35:59 -0300

    Tipando as Rotas

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,14 +1,24 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 import { SignIn } from "~/screens/SignIn";
 import { SignUp } from "~/screens/SignUp";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+type IAuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+};
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<IAuthRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<IAuthRoutes>();
 
 interface AuthRoutesProps {}
 
 export const AuthRoutes: React.FC<AuthRoutesProps> = () => (
   <Navigator screenOptions={{ headerShown: false }}>
-    <Screen name="sign-in" component={SignIn} />
-    <Screen name="sign-up" component={SignUp} />
+    <Screen name="signIn" component={SignIn} />
+    <Screen name="signUp" component={SignUp} />
   </Navigator>
 );
