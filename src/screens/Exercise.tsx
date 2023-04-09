e3486402d4bb7c17d2e733e992244f687ba61550
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 13:28:06 -0300

    Finalizando o Header do Exercício

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,7 +1,8 @@
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Icon, VStack } from "native-base";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
+import BodyIcon from "~/assets/body.svg";
 
 export interface ExerciseParams {
   id: string;
@@ -18,6 +19,20 @@ export const Exercise: React.FC<ExerciseProps> = () => {
         <TouchableOpacity activeOpacity={0.6} onPress={goBack}>
           <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
         </TouchableOpacity>
+
+        <HStack mt={3} justifyContent="space-between" alignItems="center">
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodyIcon />
+
+            <Text color="gray.200" textTransform="capitalize" ml={1}>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   );
