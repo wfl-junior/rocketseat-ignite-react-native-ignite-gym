d97c33f9330e68b632ccfe2b55eddb7babc69449
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 11:24:00 -0300

    Utilizando SectionList

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,37 @@
-import { VStack } from "native-base";
+import { Heading, SectionList, VStack } from "native-base";
+import { useState } from "react";
 import { HistoryCard } from "~/components/HistoryCard";
 import { ScreenHeader } from "~/components/ScreenHeader";
 
 interface HistoryProps {}
 
-export const History: React.FC<HistoryProps> = () => (
-  <VStack flex={1}>
-    <ScreenHeader title="Histórico de Exercícios" />
-    <HistoryCard />
-    <HistoryCard />
-    <HistoryCard />
-  </VStack>
-);
+export const History: React.FC<HistoryProps> = () => {
+  const [exercises, setExercises] = useState([
+    {
+      title: "27.08.2022",
+      data: ["Puxada frontal"],
+    },
+    {
+      title: "26.08.2022",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+  ]);
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        px={8}
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+      />
+    </VStack>
+  );
+};
