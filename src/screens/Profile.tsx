192cc554b7ead911eec89c60312a5dabb2b245df
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 12:13:26 -0300

    Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,21 @@
-import { VStack } from "native-base";
+import { Center, ScrollView, VStack } from "native-base";
 import { ScreenHeader } from "~/components/ScreenHeader";
+import { UserPhoto } from "~/components/UserPhoto";
 
 interface ProfileProps {}
 
 export const Profile: React.FC<ProfileProps> = () => (
   <VStack flex={1}>
     <ScreenHeader title="Perfil" />
+
+    <ScrollView>
+      <Center mt={6} px={10}>
+        <UserPhoto
+          size={33}
+          alt="Foto do usuário"
+          source={{ uri: "https://github.com/wfl-junior.png" }}
+        />
+      </Center>
+    </ScrollView>
   </VStack>
 );
