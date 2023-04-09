de25a59c812dada1c011cd54b550e8c0ed2274ed
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 10:18:36 -0300

    Utilizando o Pressable

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,11 +1,23 @@
-import { Heading } from "native-base";
+import { Heading, IPressableProps, Pressable } from "native-base";
 
-interface GroupProps {
+interface GroupProps extends IPressableProps {
   name: string;
 }
 
-export const Group: React.FC<GroupProps> = ({ name }) => (
-  <Heading color="gray.200" textTransform="uppercase" fontSize="xs">
-    {name}
-  </Heading>
+export const Group: React.FC<GroupProps> = ({ name, ...props }) => (
+  <Pressable
+    mr={3}
+    w={24}
+    h={10}
+    rounded="md"
+    bg="gray.600"
+    overflow="hidden"
+    alignItems="center"
+    justifyContent="center"
+    {...props}
+  >
+    <Heading color="gray.200" textTransform="uppercase" fontSize="xs">
+      {name}
+    </Heading>
+  </Pressable>
 );
