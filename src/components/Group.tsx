7245d869db1a06aa532a987f84c678d3218a3246
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 10:26:48 -0300

    Exibindo o grupo selecionado

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,9 +2,14 @@ import { Heading, IPressableProps, Pressable } from "native-base";
 
 interface GroupProps extends IPressableProps {
   name: string;
+  isActive?: boolean;
 }
 
-export const Group: React.FC<GroupProps> = ({ name, ...props }) => (
+export const Group: React.FC<GroupProps> = ({
+  name,
+  isActive = false,
+  ...props
+}) => (
   <Pressable
     mr={3}
     w={24}
@@ -14,12 +19,17 @@ export const Group: React.FC<GroupProps> = ({ name, ...props }) => (
     borderWidth={1}
     overflow="hidden"
     alignItems="center"
+    isPressed={isActive}
     borderColor="gray.600"
     justifyContent="center"
     _pressed={{ borderColor: "green.500" }}
     {...props}
   >
-    <Heading color="gray.200" textTransform="uppercase" fontSize="xs">
+    <Heading
+      fontSize="xs"
+      textTransform="uppercase"
+      color={isActive ? "green.500" : "gray.200"}
+    >
       {name}
     </Heading>
   </Pressable>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,32 @@
 import { HStack, VStack } from "native-base";
+import { useState } from "react";
 import { Group } from "~/components/Group";
 import { HomeHeader } from "~/components/HomeHeader";
 
+enum GroupType {
+  Costas = "Costas",
+  Ombro = "Ombro",
+}
+
 interface HomeProps {}
 
-export const Home: React.FC<HomeProps> = () => (
-  <VStack flex={1}>
-    <HomeHeader />
+export const Home: React.FC<HomeProps> = () => {
+  const [selectedGroup, setSelectedGroup] = useState(GroupType.Costas);
 
-    <HStack>
-      <Group name="Costas" />
-      <Group name="Ombro" />
-    </HStack>
-  </VStack>
-);
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <HStack>
+        {Object.values(GroupType).map(group => (
+          <Group
+            key={group}
+            name={group}
+            isActive={selectedGroup === group}
+            onPress={() => setSelectedGroup(group)}
+          />
+        ))}
+      </HStack>
+    </VStack>
+  );
+};
