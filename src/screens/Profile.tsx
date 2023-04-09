1175749ca94bbb7b2e695b4b1f197e9629dd4898
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 13:03:42 -0300

    Inputs de senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,7 @@
 import { Center, Heading, ScrollView, Skeleton, VStack } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import { Button } from "~/components/Button";
 import { Input } from "~/components/Input";
 import { ScreenHeader } from "~/components/ScreenHeader";
 import { UserPhoto } from "~/components/UserPhoto";
@@ -50,9 +51,39 @@ export const Profile: React.FC<ProfileProps> = () => {
             isDisabled
             variant="secondary"
             placeholder="E-mail"
-            autoCapitalize="words"
+            autoCapitalize="none"
+            keyboardType="email-address"
           />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input
+            secureTextEntry
+            variant="secondary"
+            autoCapitalize="none"
+            placeholder="Senha antiga"
+          />
+
+          <Input
+            secureTextEntry
+            variant="secondary"
+            autoCapitalize="none"
+            placeholder="Nova senha"
+          />
+
+          <Input
+            secureTextEntry
+            variant="secondary"
+            autoCapitalize="none"
+            placeholder="Confirme a nova senha"
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
