395d0c9e17e47386c6bcbb3c44f2c93ef11fb843
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 13:20:41 -0300

    Criando Botão de Voltar

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,7 @@
-import { Center, Text } from "native-base";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { Icon, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 export interface ExerciseParams {
   id: string;
@@ -6,8 +9,16 @@ export interface ExerciseParams {
 
 interface ExerciseProps {}
 
-export const Exercise: React.FC<ExerciseProps> = () => (
-  <Center flex={1}>
-    <Text color="white">Exercise</Text>
-  </Center>
-);
+export const Exercise: React.FC<ExerciseProps> = () => {
+  const { goBack } = useNavigation();
+
+  return (
+    <VStack flex={1}>
+      <VStack p={8} pt={12} bg="gray.600">
+        <TouchableOpacity activeOpacity={0.6} onPress={goBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
+  );
+};
