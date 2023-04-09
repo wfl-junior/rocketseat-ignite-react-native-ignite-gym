3fda99843e2c7e8b40fced5a08eed689a3475caa
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 10:02:26 -0300

    Estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -3,10 +3,15 @@ import { HStack, Heading, Text, VStack } from "native-base";
 interface HomeHeaderProps {}
 
 export const HomeHeader: React.FC<HomeHeaderProps> = () => (
-  <HStack>
+  <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
     <VStack ml={1}>
-      <Text color="gray.100">Olá,</Text>
-      <Heading color="gray.100">Wallace Júnior</Heading>
+      <Text color="gray.100" fontSize="md">
+        Olá,
+      </Text>
+
+      <Heading color="gray.100" fontSize="md">
+        Wallace Júnior
+      </Heading>
     </VStack>
   </HStack>
 );
