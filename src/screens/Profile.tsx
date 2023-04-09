bd3ad29b9a1a5e01946bb97803a319550e2bfce2
Wallace Júnior <wflj1997@gmail.com>
2023-04-09 12:31:32 -0300

    Efeito Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,21 +1,38 @@
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { useState } from "react";
 import { ScreenHeader } from "~/components/ScreenHeader";
 import { UserPhoto } from "~/components/UserPhoto";
 
+const photoSize = 33;
+
 interface ProfileProps {}
 
-export const Profile: React.FC<ProfileProps> = () => (
-  <VStack flex={1}>
-    <ScreenHeader title="Perfil" />
+export const Profile: React.FC<ProfileProps> = () => {
+  const [isPhotoLoading, setIsPhotoLoading] = useState(false);
 
-    <ScrollView>
-      <Center mt={6} px={10}>
-        <UserPhoto
-          size={33}
-          alt="Foto do usuário"
-          source={{ uri: "https://github.com/wfl-junior.png" }}
-        />
-      </Center>
-    </ScrollView>
-  </VStack>
-);
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {isPhotoLoading ? (
+            <Skeleton
+              w={photoSize}
+              h={photoSize}
+              rounded="full"
+              endColor="gray.600"
+              startColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              size={photoSize}
+              alt="Foto do usuário"
+              source={{ uri: "https://github.com/wfl-junior.png" }}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
+  );
+};
