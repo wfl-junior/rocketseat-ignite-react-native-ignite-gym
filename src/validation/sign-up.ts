52ed449a3652421ac4def82183450a903cf2490b
Wallace Júnior <wflj1997@gmail.com>
2023-04-21 13:44:57 -0300

    Validando senha

--- src/validation/sign-up.ts
+++ src/validation/sign-up.ts
@@ -3,7 +3,10 @@ import * as yup from "yup";
 export const signUpValidationSchema = yup.object({
   name: yup.string().required("Informe o nome"),
   email: yup.string().required("Informe o e-mail").email("E-mail inválido"),
-  password: yup.string().required("Informe a senha"),
+  password: yup
+    .string()
+    .required("Informe a senha")
+    .min(6, "A senha deve conter no mínimo 6 caracteres"),
   passwordConfirmation: yup.string().required("Confirme a senha"),
 });
 
